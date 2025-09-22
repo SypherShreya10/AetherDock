0e28e0c2ae16fe6d8d2bb013edaef4c34af091ab
Anushree-007 <anushreekendale@gmail.com>
2025-09-23 00:50:18 +0530

    backend deleted

--- Backend/index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-app.use(cors());
-app.get("/", (req,res)=>res.json({msg:"Backend working"}));
-app.listen(5000, ()=>console.log("Server on 5000"));
